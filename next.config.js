9f16cf50686027dd004594c8ae39d7ede83aa346
wimpz <wimpycliff@gmail.com>
2025-08-27 08:58:39 +0800

    config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
+// next.config.js
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
   pwa: {
     dest: 'public', // Directory where the service worker and cache will be placed
-    register: true,  // Register the service worker automatically
+    register: true, // Register the service worker automatically
     skipWaiting: true, // Activate the service worker immediately
-    disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
   },
-  reactStrictMode: true,  // Enables React Strict Mode (recommended for development)
-  experimental: {
-    turbopack: true,
-  },
+  reactStrictMode: true, // Enables strict mode for React
 });
